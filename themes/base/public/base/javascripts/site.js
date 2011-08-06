c07e1b25d0e9d765723cc5b3184fbc708a93e5c1
Adam Stacoviak <adam@stacoviak.com>
2011-08-06 03:16:20 -0500

    More changes large changes and Sass clean up. Added disqus, fusion ads and more

--- themes/base/public/base/javascripts/site.js
+++ themes/base/public/base/javascripts/site.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+  
+	// Open external links in a new window
+	hostname = window.location.hostname
+	$("a[href^=http]")
+	  .not("a[href*='" + hostname + "']")
+	  .addClass('link external')
+	  .attr('target', '_blank');
 
   $('body > header a').lettering();
 
