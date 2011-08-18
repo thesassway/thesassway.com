e0d91ecec71f55364424739da7a7d07e06f111c8
Adam Stacoviak <adam@stacoviak.com>
2011-08-18 11:46:48 -0500

    Fixed the grid toggle

--- themes/base/public/base/javascripts/site.js
+++ themes/base/public/base/javascripts/site.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   // Create, show and hide the grid
   $('body').append('<div id="grid"></div>');
-  $('body').append('<a href="#" id="toggle-grid"><span class="hide">Show Grid</span><span class="show">Hide Grid</span></a>');
+  $('body').append('<a href="#" id="toggle-grid"><span class="hide">Hide Grid</span><span class="show">Show Grid</span></a>');
   $('a#toggle-grid').toggle(function() {
     $('#grid').slideDown('fast');
     $('#toggle-grid .hide').show();
