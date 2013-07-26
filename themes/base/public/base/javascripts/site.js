cd1a28f239c41bbcee9de77018f77a3f80afe954
Adam Stacoviak <adam@stacoviak.com>
2013-07-26 14:12:42 -0500

    Removed the grid toggle

--- themes/base/public/base/javascripts/site.js
+++ themes/base/public/base/javascripts/site.js
@@ -1,18 +1,5 @@
 $(document).ready(function() {
 
-  // Create, show and hide the grid
-  $('body').append('<div id="grid"></div>');
-  $('body').append('<a href="#" id="toggle-grid"><span class="hide">Hide Grid</span><span class="show">Show Grid</span></a>');
-  $('a#toggle-grid').toggle(function() {
-    $('#grid').slideDown('fast');
-    $('#toggle-grid .hide').show();
-    $('#toggle-grid .show').hide();
-  }, function() {
-    $('#grid').slideUp('slow');
-    $('#toggle-grid .hide').hide();
-    $('#toggle-grid .show').show();
-  });
-  
   // Open external links in a new window
   hostname = window.location.hostname
   $("a[href^=http]")
